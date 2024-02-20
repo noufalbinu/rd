3a78eb00c7f9e08c5284b6b30dcfe6ea373045fb
Noufal Binu <digitcrop@gmail.com>
2024-02-20 12:28:48 +0530

    updated

--- front-page.php
+++ front-page.php
@@ -6,13 +6,20 @@
     <div class="main-banner-wrap">
         <div class="main-banner container-width-1100">
             <div class="text-content">
-                <h1>Where Innovation Fuses with Design to Breathe Life into Your WordPress Experience!</h1>
-                <p>At WPadroit, we embody a dynamic and creative spirit, boasting a team of developers and designers who are genuinely passionate about curating extraordinary themes and plugins. Our mission is to transcend the ordinary and elevate your online presence to new heights.
-                    Dedicated to delivering excellence in quality and user experience, we specialize in the art of WordPress theme and plugin development. Our goal is not merely to make your website functional, but to make it a visually stunning masterpiece that captivates and engages your audience.
-                    Step into the realm of WPadroit, where your WordPress journey becomes a seamless blend of innovation and design, transforming your digital presence into a work of art.
-                </p>
+                <h1>Explore high-quality WordPress themes tailored for diverse projects. </h1>
+                <p>Find a rich collection of customizable themes, templates, and CMS products, skillfully crafted by renowned developers to meet your specific needs.</p>
+                <div class="bnr-search">
+                    <input class="search-input" placeholder="Search Themes" type="search">
+                    <button class="search-button" placeholder="Search Themes" type="button">
+                    <svg class="bnr-search-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M24 24L20.2223 20.2156M22.3158 15.1579C22.3158 17.0563 21.5617 18.8769 20.2193 20.2193C18.8769 21.5617 17.0563 22.3158 15.1579 22.3158C13.2595 22.3158 11.4389 21.5617 10.0965 20.2193C8.75413 18.8769 8 17.0563 8 15.1579C8 13.2595 8.75413 11.4389 10.0965 10.0965C11.4389 8.75413 13.2595 8 15.1579 8C17.0563 8 18.8769 8.75413 20.2193 10.0965C21.5617 11.4389 22.3158 13.2595 22.3158 15.1579Z" stroke="white" stroke-width="2" stroke-linecap="round"/>
+                    </svg>
+                    </button>
+                </div>
             </div>
-            <div class="column"></div>
+            <div class="column bnr-img">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/crunch-themes-wordpress-free-themes-premium-themes.png" alt="">
+            </div>
         </div>
     </div>
     <div class="get-it-now ">
@@ -23,7 +30,7 @@
             <a class="cta cta-white" href="#">Get Themes</a>
         </div>
     </div>
-<!-- MAIN CONTAINER -->
+    <!-- MAIN CONTAINER -->
     <div id='main-container'>
         <div class="resources-container">
             <div class="resources container-width-1100">
@@ -44,16 +51,15 @@
                             'posts_per_page' => 5, 
                             'order' => 'DESC' 
                         );
-                         $rp = new WP_Query( $args );
-                         if($rp->have_posts()) :
-                         while($rp->have_posts()) : $rp->the_post(); ?>
-                         <div class='web'>
-                             <div class='scub'>
-                                 <div class="excerpt-image"><?php if ( has_post_thumbnail() ) { the_post_thumbnail( array(300, 200) );  } ?></div>
-                                 <h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></h3></a>
-                             </div>
-                         </div>
- 
+                        $rp = new WP_Query( $args );
+                        if($rp->have_posts()) :
+                        while($rp->have_posts()) : $rp->the_post(); ?>
+                        <div class='web'>
+                           <div class='scub'>
+                               <div class="excerpt-image"><?php if ( has_post_thumbnail() ) { the_post_thumbnail( array(300, 200) );  } ?></div>
+                               <h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></h3></a>
+                           </div>
+                        </div>
                         <?php endwhile; wp_reset_postdata(); endif;?>
                     </div>
                 </div>
--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,700&family=Lato:wght@100&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     <!----- JS ------>	
     <?php wp_enqueue_script('jquery'); ?>
